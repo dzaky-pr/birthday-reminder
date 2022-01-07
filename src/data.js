097a99a4a5dc56096e5aea25a21e574d149e4b44
Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>
2022-01-07 22:51:58 +0700

    first commit
    Signed-off-by: Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    name: 'Elon Musk',
+    age: 50,
+    image: 'https://res.cloudinary.com/dzaky/image/upload/v1641569470/elon_musk_by2yup.jpg',
+  },
+  {
+    id: 2,
+    name: 'Adamas Belva',
+    age: 31,
+    image: 'https://res.cloudinary.com/dzaky/image/upload/v1641569471/adamas_belva_seqsdk.png',
+  },
+  {
+    id: 3,
+    name: 'Maudy Ayunda',
+    age: 27,
+    image: 'https://res.cloudinary.com/dzaky/image/upload/v1641569471/maudy_ayunda_md5ift.jpg',
+  },
+  {
+    id: 4,
+    name: 'Jerome Polin',
+    age: 23,
+    image: 'https://res.cloudinary.com/dzaky/image/upload/v1641569470/jerome_polin_q1ali1.jpg',
+  },
+  {
+    id: 5,
+    name: 'Doge Fluffy',
+    age: 1,
+    image: 'https://res.cloudinary.com/dzaky/image/upload/v1641569471/doge_shiba_v2ks1o.png',
+  },
+];
